7254e0a1e12576d851fe945f1f573ba7f90881e3
Savelii Merts <3ave115Mr.s@gmail.com>
2022-03-18 19:33:04 +0300

    w

--- lab_1.cpp
+++ lab_1.cpp
@@ -3,35 +3,34 @@
 #include <iomanip>
 #include <vector>
 
+using namespace std;
+
 typedef struct Item {
-    uint16_t key; //0-UINT16_MAX
-    uint64_t value; //0-UINT63_MAX
+    uint_fast16_t key; //0-UINT16_MAX
+    uint_fast64_t value; //0-UINT63_MAX
 } Item;
 
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    std::vector<Item> in_arr;
+    vector<Item> in_arr;
     Item in_item;
-    while (std::cin >> in_item.key >> in_item.value)
-        in_arr.emplace_back(in_item);
-    
-    std::vector<uint64_t> count_arr(UINT16_MAX + 1, 0); // тип определяется количеством эллементов 
+    while (cin >> in_item.key >> in_item.value)
+        in_arr.push_back(move(in_item));
 
-    for(int i = 0; i < in_arr.size(); ++i)
-        ++count_arr[in_arr[i].key];
+    vector<uint_fast16_t> count(UINT16_MAX + 1, 0);
+    for (int i = 0; i < in_arr.size(); ++i)
+        ++count[in_arr[i].key];
 
-    for(int i = 1; i < UINT16_MAX + 1; ++i)
-        count_arr[i] += count_arr[i - 1];
+    for (int i = 1; i < count.size(); ++i)
+        count[i] +=  count[i - 1];
 
-    std::vector<Item*> out_arr(in_arr.size());
-    for(int i = in_arr.size() - 1; i >= 0; --i)
-        out_arr[--count_arr[in_arr[i].key]] = &in_arr[i];
+    vector<Item*> out_arr(in_arr.size());
+    for (int i = in_arr.size() - 1; i >= 0; --i)
+        out_arr[--count[in_arr[i].key]] = &in_arr[i];
 
-    for(int i = 0; i < in_arr.size(); ++i)
-        std::cout << out_arr[i]->key << '\t' << out_arr[i]->value << std::endl;
-
-    return 0;
+    for (int i = 0; i < in_arr.size(); i++)
+        cout << out_arr[i]->key << '\t' << out_arr[i]->value << '\n';
 }
