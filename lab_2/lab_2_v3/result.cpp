47cd064d789dfccd74aa03b0992dd626f3df65eb
Sava <3ave115mr.s@gmail.com>
2022-05-10 12:02:36 +0300

    w

--- lab_2/lab_2_v3/result.cpp
+++ lab_2/lab_2_v3/result.cpp
@@ -1,7 +1,38 @@
-#ifndef T_B_TREE_NODE_HPP
-#define T_B_TREE_NODE_HPP
+#include <cstdint>
+#include <string>
 
-#include "t_b_tree_item.hpp"
+class TBTreeItem
+{
+private:
+    std::string key;
+    uint64_t value;
+public:
+    TBTreeItem(const TBTreeItem& inItem) {
+        key = inItem.key;
+        value = inItem.value;
+    }
+    TBTreeItem(const std::string& inKey, const uint64_t& inValue) {
+        key = inKey;
+        value = inValue;
+    }
+    ~TBTreeItem() = default;
+    std::string Key() {
+        return key;
+    }
+    uint64_t Value() {
+        return value;
+    }
+    friend bool operator<(const TBTreeItem& a, const TBTreeItem& b) {
+        return (a.key.compare(b.key) < 0) ? true : false;
+    }
+    friend bool operator>(const TBTreeItem& a, const TBTreeItem& b) {
+        return (a.key.compare(b.key) > 0) ? true : false;
+    }
+    friend bool operator==(const TBTreeItem& a, const TBTreeItem& b) {
+        return (a.key.compare(b.key) == 0) ? true : false;
+    }
+};
+
 #include <vector>
 
 const uint8_t TREE_DEGREE = 2;
@@ -255,4 +286,110 @@ public:
     }
 };
 
-#endif /*T_B_TREE_NODE_HPP*/
+#include <fstream>
+
+class TBTree
+{
+private:
+    TBTreeNode* root;
+public:
+    TBTree() {
+        root = nullptr;
+    }
+    TBTree(TBTreeNode* inNode) {
+        root = inNode;
+    }
+    ~TBTree() {
+        delete root;
+    }
+    TBTreeItem* Search(const TBTreeItem& itemForSearch) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        return root->SearchInNode(itemForSearch);
+    }
+    void Insert(const TBTreeItem& itemForInsert) {
+        if (root == nullptr) {
+            root = new TBTreeNode(itemForInsert);
+            return;
+        }
+        if (root->NodeIsFull()) {
+            root = root->SplitRoot();
+        }
+        root->InsertToNode(itemForInsert);
+        if (root->NodeIsFull()) {
+            root = root->SplitRoot();
+        }
+    }
+    void Erase(const TBTreeItem& itemForErase) {
+        if (root == nullptr) {
+            return;
+        }
+        if (root->NodeIsEmpty()) {
+            root = root->FillRoot();
+        }
+        root->EraseFromNode(itemForErase);
+        if (root->NodeIsEmpty()) {
+            root = root->FillRoot();
+        }
+    }
+    void Save(const std::ofstream ToWtiteFile);
+    void Load(const std::ofstream ToLoadFile);
+};
+
+#include <iostream>
+#include <utility>
+#include <fstream>
+#include <algorithm>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::string command;
+    std::string keyWord;
+    uint64_t value;
+    TBTree tree;
+    std::ifstream in("test.txt");
+    std::ofstream out("result.txt");
+    while (in >> command) {
+        if (command == "+") {
+            in >> keyWord >> value;
+            std::transform(keyWord.begin(), keyWord.end(), keyWord.begin(), tolower);
+            TBTreeItem toInsertItem(keyWord, value);
+            if (tree.Search(toInsertItem) != nullptr) {
+                out << "Exist" << std::endl;
+            } else {
+                tree.Insert(toInsertItem);
+                out << "OK" << std::endl;
+            }
+        } else if (command == "-") {
+            in >> keyWord;
+            std::transform(keyWord.begin(), keyWord.end(), keyWord.begin(), tolower);
+            TBTreeItem toEraseItem(keyWord, 0);
+            if (tree.Search(toEraseItem) == nullptr){
+                out << "NoSuchWord" << std::endl;
+            } else {
+                tree.Erase(toEraseItem);
+                out << "OK" << std::endl;
+            }
+        } else if (command == "!") {
+            // std::string path;
+            // in >> command >> path;
+            // if (command == "Save") {
+            //     std::ofstream ToWriteFile(path, std::ios::trunc | std::ios::binary);
+            //     out << "OK\n";
+            // } else if (command == "Load") {
+            // }
+        } else {
+            std::transform(command.begin(), command.end(), command.begin(), tolower);
+            TBTreeItem toSearchItem(command, 0);
+            TBTreeItem* foundItem = tree.Search(toSearchItem);
+            if (foundItem != nullptr) {
+                out << "OK: " << foundItem->Value() << std::endl;
+            } else {
+                out << "NoSuchWord" << std::endl;
+            }
+        }
+    }
+    return 0;
+}
