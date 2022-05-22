c1a6e6eb26f9361b55a998b0ec5cc877005e772b
Sava <3ave115mr.s@gmail.com>
2022-05-23 00:12:35 +0300

    wa on 6 test

--- lab_4/z_bloc_main.cpp
+++ lab_4/z_bloc_main.cpp
@@ -1,93 +1,86 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <cstdint>
-#include <ctype.h>
 #include <sstream>
 
-class intChar
+using namespace std;
+
+struct intChar
 {
-private:
     uint32_t value;
-public:
-    uint16_t numInString, numOfString;
-    friend bool operator<(const intChar& a, const intChar& b) {
-        return (a.value < b.value) ? true : false;
-    }
-    friend bool operator==(const intChar& a, const intChar& b) {
-        return (a.value == b.value) ? true : false;
-    }
-    friend std::istream& operator>> (std::istream &in, intChar &intChar) {
-        in >> intChar.value;
-        return in;
-    }
+    uint16_t numInString;
+    uint16_t numOfString;
 };
 
-void zFunc(const std::vector<intChar>& inString, std::vector<int>& zF) {
+bool operator<(const intChar& a, const intChar& b) {
+    return (a.value < b.value) ? true : false;
+}
+bool operator==(const intChar& a, const intChar& b) {
+    return (a.value == b.value) ? true : false;
+}
+
+void zFunc(const vector<intChar>& inString, vector<int>& zFRes) {
     int strLength = inString.size();
-	zF.resize(strLength, 0);
-	for (int i = 1, l = 0, r = 0; i < strLength - 1; ++i) {
-		zF[i] = std::max(0, std::min (r - i, zF[i - l]));
-		while (i + zF[i] < strLength && inString[zF[i]] == inString[i + zF[i]])
-			++zF[i];
-		if (i + zF[i] > r) {
-			l = i;  
-            r = i + zF[i];
+	zFRes.resize(strLength, 0);
+    int leftBound = 0;
+    int rightBound = 0;
+	for (int i = 1; i < strLength - 1; ++i) {
+		zFRes[i] = max(0, min (rightBound - i, zFRes[i - leftBound]));
+		while (i + zFRes[i] < strLength && inString[zFRes[i]] == inString[i + zFRes[i]]) {
+        	++zFRes[i];    
+        }
+		if (i + zFRes[i] > rightBound) {
+			leftBound = i;  
+            rightBound = i + zFRes[i];
         }
 	}
 }
 
-void PatternRead(std::istream &in, std::vector<intChar>& pattern) {
-    intChar inChar;
-    std::string inString;
+void PatternRead(istream &in, vector<intChar>& pattern) {
+    string inString;
     getline(in, inString);
-    std::stringstream inStream(inString);
-    int wordNumber = 1;
+    stringstream inStream(inString);
+    uint32_t inChar = 1;
     while (inStream >> inChar)
     {
-        inChar.numInString = wordNumber;
-        inChar.numOfString = 1;
-        pattern.push_back(inChar);
-        wordNumber++;
+        pattern.push_back({inChar, 0, 0});
     }
 }
 
-void TextRead(std::istream &in, std::vector<intChar>& inText) {
-    intChar inChar;
-    std::string inString;
-    int lineNumber = 1;
+void TextRead(istream &in, vector<intChar>& inText) {
+    string inString;
+    uint32_t inChar;
+    uint16_t lineNumber = 1;
     while (!in.eof()) {
         getline(in, inString);
-        std::stringstream inStream(inString);
-        int wordNumber = 1;
+        stringstream inStream(inString);
+        uint16_t wordNumber = 1;
         while (inStream >> inChar) {
-            inChar.numInString = wordNumber;
-            inChar.numOfString = lineNumber;
-            inText.push_back(inChar);
+            inText.push_back({inChar, wordNumber, lineNumber});
             wordNumber++;
         }
         lineNumber++;
     };
 }
 
-void substringInTextSearch (std::istream &in, std::ostream &out) {
-    std::vector<intChar> inText;
+void substringInTextSearch (istream &in, ostream &out) {
+    vector<intChar> inText;
     PatternRead(in, inText);
     uint16_t patternSize = inText.size();
     TextRead(in, inText);
 
-    std::vector<int> zFuncRes;
+    vector<int> zFuncRes;
     zFunc(inText, zFuncRes);
 
     for (uint16_t i = patternSize; i < inText.size(); ++i) {
-        if (zFuncRes[i] >= patternSize) {
+        if (zFuncRes[i] >= patternSize) { // вот тут ошибка должно быть ==
             out << inText[i].numOfString << ", " << inText[i].numInString << '\n';
         }
     }
 }
 
 int main () {
-    substringInTextSearch(std::cin, std::cout);
+    substringInTextSearch(cin, cout);
     return 0;
 }
